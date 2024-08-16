1b388248623812b7871669d39f3afe48f8e0f2f7
Renata Rainho <renata.rainho95@gmail.com>
2024-08-16 16:38:39 -0300

    :construction: Implementando os componentes e incluindo o CRUD

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,64 @@
-import React from 'react';
-import Home from './paginas/home/Home';
+// Elemento JSX/TSX => Função que retorna HTML
+//import Flex from "./components/Flex/Flex";
+// import UserProvider from './Contexts/UserContext';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
-function App() {
+import axios from 'axios';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+
+import 'react-toastify/dist/ReactToastify.css';
+import ListaCategorias from './components/Categorias/ListarCategoria/ListaCategoria';
+import FormularioCategoria from './components/Categorias/FormularioCategoria/FormularioCategoria';
+import DeletarCategoria from './components/Categorias/DeletarCategoria/DeletarCategoria';
+
+// interface User {
+// id: number;
+// name: string;
+
+// }
+
+// export default function App() {
+//   const [users, setUsers] = useState<User[]>([]);
+
+//   useEffect(() => {
+//     axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+//             .then(response =>{
+//               setUsers(response.data);
+//             }).catch(error => {console.log(error);
+
+//             })
+//   },[])
+export default function App() {
   return (
-   <Home />
+    <>
+     
+      <BrowserRouter>
+      <Navbar />
+      <div className="min-h-[80vh] bg-[#e08025] text-[#753219]">  
+        <Routes>
+           <Route path="/" element={<Home />} /> 
+           <Route path="/home" element={<Home />} />
+           <Route path="/about" element={<About />} />
+           <Route path="/contact" element={<Contact />} />
+           <Route path="/categorias" element={<ListaCategorias />} />
+           <Route path="/editarCategoria/:id" element={<FormularioCategoria/>}></Route>
+           <Route path="/cadastrarCategorias" element={<FormularioCategoria/>}></Route>
+           <Route path="/deletarCategoria/:id" element={<DeletarCategoria/>}></Route>
+          
+        </Routes>
+         </div>
+      <Footer />
+    </BrowserRouter>
+    
+    
+    </>
   );
 }
 
-export default App;
---
+++ src/pages/About/About.tsx
@@ -0,0 +1,37 @@
+export default function About() {
+  return (
+    <>
+      <h1
+        style={{
+          textAlign: "center",
+          paddingTop: "30px",
+          paddingBottom: "5px",
+          fontSize: "30px",
+          fontWeight: "bold",
+          display: 'flex-center'
+        }}
+      >
+       Sobre nós
+      </h1>
+     
+      <div
+        style={{
+          paddingTop: "50px",
+          paddingBottom: "50px",
+          fontSize: "20px",
+          fontWeight: "unset",
+        }}
+      >
+        <p style={{ textAlign: "center" }}>
+         Somos uma farmácia especializada em produtos para pele. 
+        </p>
+        <p style={{ textAlign: "center" }}>
+          Temos parceria com a rede Onodera Estética e nossos produtos contam 
+          com a qualidade e excelência para que seu cuidado com a pele esteja sempre em dia.
+          </p>
+          
+        
+      </div>
+    </>
+  );
+}
